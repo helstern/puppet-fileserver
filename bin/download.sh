e63ed0c2d2d368d0d85ec5b896443c8503e1538f
Radu Helstern <radu.helstern@gmail.com>
2014-12-19 05:41:59 +0000

    create download files script

--- bin/download.sh
+++ bin/download.sh
@@ -1,8 +1,37 @@
-#!/bin/bash
+#!/bin/bash -e
+
+#set -x 
 
 DIR_BIN=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-DIR_DOWNLOAD_ROOT=$(cd "${DIR_BIN}/../www" && pwd)
+FILESERVER_ROOT=$(cd "${DIR_BIN}/../www" && pwd)
+
+#######################################
+# use wget to download a file to a dir under fileserver root
+# Globals:
+#   FILESERVER_ROOT
+# Arguments:
+#   File Url
+#   Directory under fileserver root
+#   Message to output
+# Returns:
+#   None
+#######################################
+function wget_download()
+{
+    local WGETURL=$1
+    local DIRPREFIX="${FILESERVER_ROOT}/${2}"
+    local MESSAGE="${3-$WGETURL}"
+
+    echo -en "${MESSAGE}    "
+
+    wget  --progress=dot:mega \
+        --config="${DIR_BIN}/download.wgetrc" \
+        --no-directories --directory-prefix "${DIRPREFIX}" \
+        ${WGETURL} 2>&1 | grep --line-buffered -o "[0-9]*%" | xargs -L1 echo -en "\b\b\b\b"
+
+        #sed -u -e "s,\.,,g" | awk '{printf("\b\b\b\b%4s", $2)}'
+    echo " DONE"
+}
 
 WGETURL=http://www.mirrorservice.org/sites/ftp.apache.org/maven/maven-3/3.2.3/binaries/apache-maven-3.2.3-bin.tar.gz
-echo "downloading apache-maven from ${WGETURL}"
-wget  --config "${DIR_BIN}/download.wgetrc"  --no-directories --directory-prefix "${DIR_DOWNLOAD_ROOT}\common" ${WGETURL}
+wget_download "${WGETURL}" "common" "downloading apache-maven-3.2.3-bin.tar.gz"
