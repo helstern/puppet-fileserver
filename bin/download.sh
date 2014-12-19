688340ece2e6dcb15257f9ddd175e8b1f6036046
Radu Helstern <radu.helstern@gmail.com>
2014-12-19 15:56:17 +0000

    add maven sources to download scripr

--- bin/download.sh
+++ bin/download.sh
@@ -33,6 +33,10 @@ function wget_download()
     echo " DONE"
 }
 
-# apache maven
-WGETURL=http://www.mirrorservice.org/sites/ftp.apache.org/maven/maven-3/3.2.3/binaries/apache-maven-3.2.3-bin.tar.gz
+# apache maven binary
+WGETURL=http://mirror.gopotato.co.uk/apache/maven/maven-3/3.2.3/binaries/apache-maven-3.2.3-bin.tar.gz
+wget_download "${WGETURL}" "common"
+
+# apache maven source
+WGETURL=http://mirror.gopotato.co.uk/apache/maven/maven-3/3.2.3/source/apache-maven-3.2.3-src.tar.gz
 wget_download "${WGETURL}" "common"
