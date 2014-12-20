04459222827f1b8d14d29826258e2dffa9b28d82
Radu Helstern <radu.helstern@gmail.com>
2014-12-20 02:49:30 +0000

    change initial maven version to 3.2.5

--- bin/download.sh
+++ bin/download.sh
@@ -34,9 +34,9 @@ function wget_download()
 }
 
 # apache maven binary
-WGETURL=http://mirror.gopotato.co.uk/apache/maven/maven-3/3.2.3/binaries/apache-maven-3.2.3-bin.tar.gz
+WGETURL=http://mirror.gopotato.co.uk/apache/maven/maven-3/3.2.5/binaries/apache-maven-3.2.5-bin.tar.gz
 wget_download "${WGETURL}" "modules/maven"
 
 # apache maven source
-WGETURL=http://mirror.gopotato.co.uk/apache/maven/maven-3/3.2.3/source/apache-maven-3.2.3-src.tar.gz
+WGETURL=http://mirror.gopotato.co.uk/apache/maven/maven-3/3.2.5/source/apache-maven-3.2.5-src.tar.gz
 wget_download "${WGETURL}" "modules/maven"
