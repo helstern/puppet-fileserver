3086c959ce719af6ea10a3baf2f40315220e1141
Radu Helstern <radu.helstern@gmail.com>
2014-12-22 00:58:37 +0000

    rename maven folder to apache_maven

--- bin/download.sh
+++ bin/download.sh
@@ -29,14 +29,13 @@ function wget_download()
         --no-directories --directory-prefix "${DIRPREFIX}" \
         ${WGETURL} 2>&1 | grep --line-buffered -o "[0-9]*%" | xargs -L1 echo -en "\b\b\b\b"
 
-        #sed -u -e "s,\.,,g" | awk '{printf("\b\b\b\b%4s", $2)}'
     echo " DONE"
 }
 
 # apache maven binary
 WGETURL=http://mirror.gopotato.co.uk/apache/maven/maven-3/3.2.5/binaries/apache-maven-3.2.5-bin.tar.gz
-wget_download "${WGETURL}" "modules/maven"
+wget_download "${WGETURL}" "modules/apache_maven"
 
 # apache maven source
 WGETURL=http://mirror.gopotato.co.uk/apache/maven/maven-3/3.2.5/source/apache-maven-3.2.5-src.tar.gz
-wget_download "${WGETURL}" "modules/maven"
+wget_download "${WGETURL}" "modules/apache_maven"
